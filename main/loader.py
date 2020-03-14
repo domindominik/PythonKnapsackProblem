9b676902068d7b29ff46a477a69abf9133634c4a
dominik <dominwro@gmail.com>
2020-03-14 14:27:41 +0100

    Bug fix and the code refactor.

--- main/loader.py
+++ main/loader.py
@@ -6,11 +6,7 @@ class Loader:
         self.Knapsack = Knapsack
 
     def loading(self, items_list):
-
         items_list = sorted(items_list, key=lambda item: item.profitability, reverse=True)
-
         for x in items_list:
-            #if Knapsack.max_weight >= Knapsack.knapsack_weight + Item.weight:
-            if Knapsack.max_weight > Knapsack.knapsack_weight + Item.weight:
-                Knapsack.add_item(Item)
-                #Knapsack.knapsack_weight = Item.weight + Knapsack.knapsack_weight
+            if self.Knapsack.max_weight >= self.Knapsack.knapsack_weight + x.weight:
+                self.Knapsack.add_item(x)
--- main/menu.py
+++ main/menu.py
@@ -9,30 +9,22 @@ items_list.append(Item("seba", 5, 100))
 items_list.append(Item("seba", 25, 170))
 items_list.append(Item("lara", 35, 10))
 items_list.append(Item("lara", 15, 16))
+items_list.append(Item("lara croft", 215, 916))
+items_list.append(Item("iphone", 21, 191))
+items_list.append(Item("iphone 11 pro", 20.01, 400.9))
+items_list.append(Item("iphone 11 pro", 10.01, 100.9))
 
 print("Items before sorting")
 
 for x in items_list:
     x.print_info()
 
-knapsack = Knapsack(60)
-print(knapsack.max_weight)
-elo = Item("elobolo", 34, 987)
-#elo.print_info()
-#knapsack.add_item(elo)
 
-knapsack.print_info()
-
+knapsack = Knapsack(602)
 
 loader = Loader(knapsack)
-
-
-
-
-
 loader.loading(items_list)
 
+print("Knapsack includes")
 knapsack.print_info()
 
-print(knapsack.knapsack_weight)
-#print(knapsack.get_items_list())
